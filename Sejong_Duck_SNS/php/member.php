5b105fd81e16951312063652ddb32715269685e7
YIHOGU <45706618+YIHOGU@users.noreply.github.com>
2019-06-12 13:37:17 +0900

    Update member.php

--- Sejong_Duck_SNS/php/member.php
+++ Sejong_Duck_SNS/php/member.php
@@ -1,26 +1,27 @@
 <?php
-//테스트해봐야함
-//error_reporting(E_ALL);
-//ini_set("display_errors",1);
 
 include "db.php";
 
 $loginId=$_POST['loginId'];
 $email=$_POST['email'].'@'.$_POST['emaddress'];
 $password=$_POST['password'];
+include "password.php";
+$hash = password_hash($password,PASSWORD_DEFAULT);
 $name=$_POST['name'];
 $joinDate=date("Y-m-d");
 
 $select = "SELECT * FROM USER WHERE loginId = '$loginId'";
 $result=$conn->query($select);
-if($result->num_rows==1){
+$row_num = $result->num_rows;
+if($result > 0){
   echo "<script>alert('중복된 아이디가 존재합니다.');</script>";
-  //중복계정 생성 예외합니다.
+  echo '<script>history.back();</script>';
+  exit;
 }
 
 $insert = "INSERT INTO
 USER(loginId,email,password,name,joinDate,state)
-VALUES ('$loginId','$email','$password','$name','$joinDate',0)";
+VALUES ('$loginId','$email','$hash','$name','$joinDate',0)";
 
 if(!mysqli_query($conn, $insert)) {
 die("가입 에러 : " .mysqli_error($conn));
