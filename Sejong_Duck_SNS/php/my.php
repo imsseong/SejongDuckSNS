56c1005416abee4b323ff925bf2460c7e1f45c88
YIHOGU <45706618+YIHOGU@users.noreply.github.com>
2019-06-12 21:10:01 +0900

    Add files via upload

--- Sejong_Duck_SNS/php/my.php
+++ Sejong_Duck_SNS/php/my.php
@@ -4,9 +4,9 @@
   <meta charset='utf-8'>
   <title>꽥꽥이</title>
 </head>
-<link rel='stylesheet' href='../css/basicstyle.css'>
-<link rel='stylesheet' href='../css/search.css'>
-<link rel='stylesheet' href='../css/my.css'>
+<link rel='stylesheet' type='text/css' href='../css/basicstyle.css?alter'>
+<link rel='stylesheet' type='text/css' href='../css/search.css?alter'>
+<link rel='stylesheet' type='text/css' href='../css/my.css?alter'>
 <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.8.2/css/all.css' integrity='sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay' crossorigin='anonymous'>
 <script type="text/javascript" src="http://code.jquery.com/jquery-3.2.0.min.js"></script>
 
@@ -92,9 +92,10 @@ $num = mysqli_num_rows($result);
 $row = mysqli_fetch_assoc($result);
 if($num) { //select row 있으면
 $loginId = $row['loginId']; //post부분에서 보여질 글쓴이 아이디
+$profile = $row['profile'];
 ?>
 
-<table align='center'>
+<table align='center'> <!-- user의 프로필 사진, 게시물 수, 친구 수, 프로필 수정으로 이동 버튼, 프로필 정보 보여주기 -->
   <thead align='center'>
     <tr>
       <td rowspan='2' id='td_photo'><?php echo "<img src = '../img/profile/".$row['profile']."' style='border-radius: 50%;'>" ?></td>
@@ -129,11 +130,11 @@ $loginId = $row['loginId']; //post부분에서 보여질 글쓴이 아이디
 $postingType = 0;
 ?>
 
-<table align='center'>
+<table align='center'> <!-- 게시물을 보는 형식, 두가지 중 하나를 선택할 수 있도록 -->
   <thead align='center'>
     <tr>
-      <td style='width:50%;'><input type='button' id='btn_1' value='사진만' onclick='change1();'/></td>
-      <td><input type='button' id='btn_2' value='타임라인처럼' onclick='change2();'/></td>
+      <td style='width:50%;'><input type='button' id='btn_1' value='' style='background: url("../img/view1.jpg") no-repeat; width:32px; height:32px;' onclick='change1();'/></td>
+      <td><input type='button' id='btn_2' value='' style='background: url("../img/view2.jpg") no-repeat; width:32px; height:32px;' onclick='change2();'/></td>
     </tr>
   </thead>
 </table>
@@ -149,6 +150,7 @@ $num = mysqli_num_rows($result);
 if($num) { //select row 있으면
 
 ?>
+<!-- 게시물 보는 형식 첫번째 : 사진만 보이도록 -->
   <div id='my1' style='display:block;'>
     <table align='center'>
       <thead align='center'>
@@ -185,8 +187,9 @@ if($num) { //select row 있으면
       </table>
     </div>
 
+<!-- 게시물 보는 형식 두번째 : 사진과 글 모두 보이도록(타임라인처럼) -->
   <div id='my2' style='display:none;'>
-    <?php mysqli_data_seek($result, 0) //인덱스 0으로?>
+    <?php mysqli_data_seek($result, 0)?><!-- select한 포스트 결과의 인덱스를 0으로 -->
     <table align='center'>
     <thead align='center'>
 
@@ -196,7 +199,15 @@ if($num) { //select row 있으면
     ?>
       <tr><!-- my페이지, post 글쓴이 -->
         <td style='border-top:2px solid teal;'>
-          <b style='float:left;'><?php echo $loginId ?></b>
+          <b style='float:left; display:inline;'><?php echo "<img src='../img/profile/".$profile."' id='mini_profile'>".$loginId ?></b>
+          <form class="edit-form" action="../modify.html" method="post">
+            <input type="hidden" name="postId" value="<?php echo $row['pId'];?>">
+            <input type="submit" class="modify-button" value="">
+          </form>
+          <form class="edit-form" action="delete.php" method="post">
+            <input type="hidden" name="postId" value="<?php echo $row['pId'];?>">
+            <input type="submit" class="delete-button" value="">
+          </form>
         </td>
       </tr>
       <?php
@@ -205,7 +216,7 @@ if($num) { //select row 있으면
       ?>
       <tr><!-- post 이미지 -->
         <td>
-          <?php echo "<img src = '../img/upload/".$row['url']."' style='width:100%; height:300px;'>" ?>
+          <?php echo "<img src = '../img/upload/".$row['url']."' style='width:70%; height:auto;'>" ?>
         </td>
       </tr>
       <?php
@@ -234,11 +245,15 @@ if($num) { //select row 있으면
           <?php echo $row['content'] ?>
         </td>
       </tr>
-      <tr>
+      <tr> <!-- 댓글 내용 -->
         <td>
           <hr/>
+          <form id='reply_form'>
+          <input type='text' name='reply' placeholder='댓글 달기'>
+          <input type='hidden' id='pId' name='postId' value='<?php echo $row["pId"];?>'>
+          <input type='submit' id='reply_btn' value='게시' style='color:blue; width:10%; floar:right;'>
+          <div id='replies'>
           <?php
-
           $select = "SELECT r.*, u.loginId FROM REPLY AS r INNER JOIN USER as u USING (uId) WHERE pId=$pId";
           $res = mysqli_query($conn, $select);
           $nums = mysqli_num_rows($res);
@@ -258,7 +273,8 @@ if($num) { //select row 있으면
 
 
           ?>
-
+          </div>
+        </form>
         </td>
       </tr>
 
@@ -279,7 +295,7 @@ mysqli_close($conn);
 
     <div class='right-navigation'>
       <a href='../home.html'>
-        <p>타임라인</p>
+        <p>홈</p>
       </a>
       <a href='../post.html'>
         <p>POST</p>
@@ -294,8 +310,8 @@ mysqli_close($conn);
 
 
 
-    <footer id='main-footer'>
-      here is footer <br>
+    <footer id="main-footer">
+      copyright 2019. 홍성임, 이호진. All rights reserved<br>
     </footer>
 
   </div>
@@ -305,15 +321,55 @@ mysqli_close($conn);
 
 <script type="text/javascript">
 
-function change1() {
+function change1() { //게시물 보는 형식 첫번째 버튼 클릭시
   document.getElementById("my1").style.display="block";
   document.getElementById("my2").style.display="none";
 }
 
-function change2() {
+function change2() { //게시물 보는 형식 두번째 버튼 클릭시
   document.getElementById("my1").style.display="none";
   document.getElementById("my2").style.display="block";
 }
+
+$(document).ready(function(){
+		showReply();
+});
+
+var str="";
+function showReply(){
+		var pId = $("#pId").val();
+		$.getJSON("reply_list.php?pId="+pId, function(data){
+			console.log(data);
+
+			$(data).each(function(){
+				console.log(data);
+
+				str += "writer : "+this.writer+"<br>" +
+					"content : " +
+					this.content + "<br>";
+			});
+
+
+			$("#replies").html(str);
+		});
+}
+
+
+$(document).on("click", "#reply_btn", function() {
+
+    var formData = $("#reply_form").serialize();
+
+    $.ajax({
+      type : 'POST',
+      url : 'reply.php',
+      data : formData,
+      success : function(response) {
+        alert("성공~~~~~~~~~~~~");
+        alert(response);
+      }
+    });
+
+});
 
 
 </script>
